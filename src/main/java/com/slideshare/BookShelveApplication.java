a0ffb8769e7d8a389f0edb6d65ff6b4ecbd3dc93
amo95 <jameaamo@gmail.com>
2024-03-08 13:17:25 +0000

    added and modify config for token key

--- src/main/java/com/slideshare/BookShelveApplication.java
+++ src/main/java/com/slideshare/BookShelveApplication.java
@@ -1,9 +1,12 @@
 package com.slideshare;
 
+import com.slideshare.config.TokenConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TokenConfigProperties.class)
 public class BookShelveApplication {
 
     public static void main(String[] args) {
